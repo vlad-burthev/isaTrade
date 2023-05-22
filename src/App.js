4b8cc79fed95f604885ba9d85d6b63d5026debc7
Vlad <burthev04@gmail.com>
2023-05-22 15:35:01 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -45,7 +45,7 @@ function App() {
       <MobHeader />
 
       <CoffeeContextProvider>
-        <HashRouter>
+        <HashRouter basename={process.env.PUBLIC_URL}>
           <Routes>
             <Route
               path="/"
